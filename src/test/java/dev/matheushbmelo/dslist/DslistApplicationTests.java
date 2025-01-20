41e05fec16c6ab052224aa32aa71db1151a9267d
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-20 10:39:47 -0300

    feat: initial project commit

---
+++ src/test/java/dev/matheushbmelo/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheushbmelo.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
