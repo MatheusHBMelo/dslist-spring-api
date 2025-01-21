e7b49fecddc872a3027df36cbaaecc32bcfdedd1
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-21 15:09:43 -0300

    feat: adds the endpoint of finding a game by ID

--- src/main/java/dev/matheushbmelo/dslist/controller/GameController.java
+++ src/main/java/dev/matheushbmelo/dslist/controller/GameController.java
@@ -1,9 +1,11 @@
 package dev.matheushbmelo.dslist.controller;
 
+import dev.matheushbmelo.dslist.dtos.GameDto;
 import dev.matheushbmelo.dslist.dtos.GameMinDto;
 import dev.matheushbmelo.dslist.services.GameService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<GameMinDto>> findAll() {
         return ResponseEntity.ok(gameService.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<GameDto> findById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(gameService.findById(id));
     }
 }
