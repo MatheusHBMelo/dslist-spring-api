1c3f435e701f8f0d0247f9f15d29c910a595b18f
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-21 15:10:20 -0300

    feat: creates the dto to return a Game by id

---
+++ src/main/java/dev/matheushbmelo/dslist/dtos/GameDto.java
@@ -0,0 +1,11 @@
+package dev.matheushbmelo.dslist.dtos;
+
+import dev.matheushbmelo.dslist.entities.Game;
+
+public record GameDto(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+                      String shortDescription, String longDescription) {
+    public GameDto(Game entity) {
+        this(entity.getId(), entity.getTitle(), entity.getYear(), entity.getGenre(), entity.getPlatforms(),
+                entity.getScore(), entity.getImgUrl(), entity.getShortDescription(), entity.getLongDescription());
+    }
+}
