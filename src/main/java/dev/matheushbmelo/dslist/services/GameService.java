d461908de619d37abd4d8d3c1835a402efba9fed
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-21 21:26:51 -0300

    feat: Adds findByList method and transforms all methods into spring transactions.

--- src/main/java/dev/matheushbmelo/dslist/services/GameService.java
+++ src/main/java/dev/matheushbmelo/dslist/services/GameService.java
@@ -4,6 +4,7 @@ import dev.matheushbmelo.dslist.dtos.GameDto;
 import dev.matheushbmelo.dslist.dtos.GameMinDto;
 import dev.matheushbmelo.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,11 +16,18 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         return gameRepository.findAll().stream().map(GameMinDto::new).toList();
     }
 
+    @Transactional(readOnly = true)
     public GameDto findById(Long id) {
         return gameRepository.findById(id).map(GameDto::new).orElseThrow(() -> new RuntimeException("Game not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findByList(Long listId) {
+        return gameRepository.searchByList(listId).stream().map(GameMinDto::new).toList();
     }
 }
