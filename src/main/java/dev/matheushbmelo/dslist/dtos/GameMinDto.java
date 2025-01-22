6a8dc449167091aa029e8317914881dc24cacd20
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-21 21:25:29 -0300

    feat: adds constructor support for the data projection interface.

--- src/main/java/dev/matheushbmelo/dslist/dtos/GameMinDto.java
+++ src/main/java/dev/matheushbmelo/dslist/dtos/GameMinDto.java
@@ -1,9 +1,14 @@
 package dev.matheushbmelo.dslist.dtos;
 
 import dev.matheushbmelo.dslist.entities.Game;
+import dev.matheushbmelo.dslist.projections.GameMinProjection;
 
 public record GameMinDto(Long id, String title, Integer year, String imgUrl, String shortDescription) {
     public GameMinDto(Game entity) {
         this(entity.getId(), entity.getTitle(), entity.getYear(), entity.getImgUrl(), entity.getShortDescription());
+    }
+
+    public GameMinDto(GameMinProjection projection) {
+        this(projection.getId(), projection.getTitle(), projection.getYear(), projection.getImgUrl(), projection.getShortDescription());
     }
 }
